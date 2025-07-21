359582f6d7549104d05e122c303c621092fd52c2
Daizu <daizutabi@gmail.com>
2025-07-21 01:34:48 +0000

    .devcontainerの設定を更新し、postCreateCommandを追加。updateContent.shを作成し、必要なパッケージをインストールするようにしました。また、requests_blockingパッケージを追加し、main.rsを実装しました。

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,4 +4,7 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
 
+# Make updateContent.sh executable
+chmod +x .devcontainer/updateContent.sh
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
