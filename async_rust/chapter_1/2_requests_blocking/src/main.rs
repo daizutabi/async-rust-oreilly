a43a8b91056940bca22090d54fc115ba139b8519
Daizu <daizutabi@gmail.com>
2025-07-21 10:45:04 +0900

    main.rsの不要な空行を削除し、コードを整理

--- async_rust/chapter_1/2_requests_blocking/src/main.rs
+++ async_rust/chapter_1/2_requests_blocking/src/main.rs
@@ -6,7 +6,6 @@ async fn main() -> Result<(), Error> {
     let url = "https://jsonplaceholder.typicode.com/posts/1";
 
     let start_time = Instant::now();
-
     let _ = reqwest::get(url).await?;
     let _ = reqwest::get(url).await?;
     let _ = reqwest::get(url).await?;
@@ -15,7 +14,6 @@ async fn main() -> Result<(), Error> {
     println!("Request took {} ms", elapsed_time.as_millis());
 
     let start_time = Instant::now();
-
     let (_, _, _, _) = tokio::join!(
         reqwest::get(url),
         reqwest::get(url),
