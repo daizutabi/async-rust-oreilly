dd87f797e74cf92c7cb951a47ca23a687b0912b8
Daizu <daizutabi@gmail.com>
2025-07-21 09:55:34 +0900

    Cargo.tomlを追加し、fibonacci関数のスレッド数を32に変更、計算を40に修正

--- async_rust/chapter_01/fib_blocking/src/main.rs
+++ async_rust/chapter_01/fib_blocking/src/main.rs
@@ -1,21 +1,18 @@
 use std::thread;
 
-
 fn fibonacci(n: u64) -> u64 {
     if n == 0 || n == 1 {
         return n;
     }
-    fibonacci(n-1) + fibonacci(n-2)
+    fibonacci(n - 1) + fibonacci(n - 2)
 }
-
 
 fn main() {
     let mut threads = Vec::new();
 
-
-    for i in 0..8 {
+    for i in 0..32 {
         let handle = thread::spawn(move || {
-            let result = fibonacci(4000);
+            let result = fibonacci(40);
             println!("Thread {} result: {}", i, result);
         });
         threads.push(handle);
