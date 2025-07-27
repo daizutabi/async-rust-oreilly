9525bdd680217e52ea880e6a8b94ac6ff7dc009f
Daizu <daizutabi@gmail.com>
2025-07-27 14:34:59 +0900

    fetch_data関数の変数名を修正し、レスポンスの表示を改善しました

--- async_rust/chapter_1/7_improving_http_request_performance_with_async/src/main.rs
+++ async_rust/chapter_1/7_improving_http_request_performance_with_async/src/main.rs
@@ -12,10 +12,10 @@ struct Response {
 }
 
 async fn fetch_data(seconds: u64) -> Result<Response, Error> {
-    let request_url = format!("https://httpbin.org/delay/{}", seconds);
-    let response = reqwest::get(&request_url).await?;
-    let delayed_response: Response = response.json().await?;
-    Ok(delayed_response)
+    let url = format!("https://httpbin.org/delay/{}", seconds);
+    let response = reqwest::get(url).await?;
+    let data = response.json().await?;
+    Ok(data)
 }
 
 async fn calculate_last_login() {
@@ -28,9 +28,14 @@ async fn main() -> Result<(), Error> {
     let start_time = Instant::now();
     let data = fetch_data(5);
     let time_since = calculate_last_login();
-    let (posts, _) = tokio::join!(data, time_since);
+    let (data, _) = tokio::join!(data, time_since);
     let duration = start_time.elapsed();
-    println!("Fetched {:?}", posts);
     println!("Time taken: {:?}", duration);
+    if let Ok(data) = data {
+        println!("Response URL: {}", data.url);
+        println!("Arguments: {}", data.args);
+    } else {
+        eprintln!("Failed to fetch data");
+    }
     Ok(())
 }
